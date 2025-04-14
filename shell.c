acdc9709b602f25ba751c65fd3205a7e46632c33
razan <razan_8801@icloud.com>
2025-04-14 19:19:40 +0000

    Handle PATH and fork() for invalid commands

--- shell.c
+++ shell.c
@@ -8,6 +8,44 @@
 extern char **environ;
 
 /**
+ * find_command_path - Searches for a command in the PATH variable.
+ * @command: The command to search for.
+ *
+ * Return: The full path if found, otherwise NULL.
+ */
+char *find_command_path(char *command)
+{
+    char *path, *token, *full_path;
+    int path_length;
+
+    path = getenv("PATH");
+    if (!path)
+        return NULL;
+
+    token = strtok(path, ":");
+    while (token)
+    {
+        path_length = strlen(token) + strlen(command) + 2;
+        full_path = malloc(path_length);
+
+        if (!full_path)
+        {
+            perror("Allocation error");
+            exit(EXIT_FAILURE);
+        }
+
+        sprintf(full_path, "%s/%s", token, command);
+        if (access(full_path, X_OK) == 0)
+            return full_path;
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+
+    return NULL;
+}
+
+/**
  * tokenize_command - Splits a string into an array of arguments.
  * @buffer: The string to tokenize.
  *
@@ -16,7 +54,7 @@ extern char **environ;
 char **tokenize_command(char *buffer)
 {
     char *token;
-    char **args = malloc(10 * sizeof(char *)); /* Allocate memory */
+    char **args = malloc(10 * sizeof(char *));
     int i = 0;
 
     if (!args)
@@ -43,36 +81,51 @@ char **tokenize_command(char *buffer)
  */
 int main(void)
 {
-    char *buffer = NULL;
+    char *buffer = NULL, *command_path;
     size_t bufsize = 0;
     ssize_t characters;
     pid_t pid;
     int status;
-    int is_interactive = isatty(STDIN_FILENO); /* Check interactive mode */
+    int is_interactive = isatty(STDIN_FILENO); /* Detect interactive mode */
+    char **args; /* Declare variables at start of block */
 
-    while (1) /* Loop to continuously run the shell */
+    while (1)
     {
-        if (is_interactive) /* Display prompt only in interactive mode */
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
+        if (is_interactive)
+            write(STDOUT_FILENO, ":) ", 3); /* Updated prompt */
 
-        characters = getline(&buffer, &bufsize, stdin); /* Read user input */
-
-        if (characters == -1) /* Handle EOF (Ctrl+D) */
+        characters = getline(&buffer, &bufsize, stdin);
+        if (characters == -1)
         {
             if (is_interactive)
                 write(STDOUT_FILENO, "\n", 1);
             break;
         }
 
-        buffer[characters - 1] = '\0'; /* Remove newline character */
+        buffer[characters - 1] = '\0';
 
-        if (strlen(buffer) == 0) /* Ignore empty input */
+        if (strlen(buffer) == 0)
             continue;
 
-        if (strcmp(buffer, "exit") == 0) /* Exit the shell */
+        if (strcmp(buffer, "exit") == 0)
             break;
 
-        pid = fork(); /* Create a child process */
+        args = tokenize_command(buffer);
+
+        /* If command is an absolute or relative path, use it directly */
+        if (access(args[0], X_OK) == 0)
+            command_path = args[0];
+        else
+            command_path = find_command_path(args[0]);
+
+        if (!command_path) /* Prevent fork() if command doesn't exist */
+        {
+            fprintf(stderr, "%s: command not found\n", args[0]);
+            free(args);
+            continue;
+        }
+
+        pid = fork();
         if (pid == -1)
         {
             perror("Error:");
@@ -80,9 +133,7 @@ int main(void)
         }
         if (pid == 0) /* Child process */
         {
-            char **args = tokenize_command(buffer); /* Tokenize input */
-
-            if (execve(args[0], args, environ) == -1) /* Execute command */
+            if (execve(command_path, args, environ) == -1)
             {
                 perror("./shell");
                 free(args);
@@ -91,10 +142,14 @@ int main(void)
         }
         else /* Parent process */
         {
-            wait(&status); /* Wait for the child process */
+            wait(&status);
         }
+
+        if (command_path != args[0]) /* Free memory only if allocated */
+            free(command_path);
+        free(args);
     }
 
-    free(buffer); /* Free allocated buffer */
+    free(buffer);
     return (0);
 }
